2028d52b040d6ef84bd01fa44471791450e33939
Kjersti Berg <52451029+kjeberg@users.noreply.github.com>
2022-03-30 14:48:23 +0200

    Updated names and added units

--- PVgenerator/pvgenerator.py
+++ PVgenerator/pvgenerator.py
@@ -3,19 +3,19 @@ import pandas as pd
 
 def generate_pv_series(temperature:pd.DataFrame, irradiance:pd.DataFrame, 
     parameters:dict={
-        'Pmpp': 8.18 * 31.2, # maximum power point = maximum power point current * maximum power point voltage
-        'FF': (8.18 * 31.2)/(8.89 * 37.8), # fill factor = maximum power point / (short circuit current * open source voltage)
-        'Voc': 37.8, # open circuit current
-        'Isc': 8.89, # short circuit current
-        'T0': 25+273.15, # standard module temperature
-        'E0': 1000, # standard irradiance
-        'eta_inv': 0.9, # inverter efficiency
-        'NOCT': 45.7, # nominal operating cell temperature
+        'Pmpp': 8.18 * 31.2, # maximum power point  = maximum power point current [A] * maximum power point voltage [V]
+        'FF': (8.18 * 31.2)/(8.89 * 37.8), # fill factor = maximum power point [W] / (short circuit current [A] * open source voltage [V])
+        'Voc': 37.8, # open circuit current [V]
+        'Isc': 8.89, # short circuit current [A]
+        'T0': 25+273.15, # standard module temperature [K]
+        'E0': 1000, # standard irradiance [W/m^2]
+        'eta_inv': 0.9, # inverter efficiency [-]
+        'NOCT': 45.7, # nominal operating cell temperature [degreeC]
         'module_area': 1.6, # size of module
         'module_number':1 # amount of modules
     }, measurementfilter:dict={
-        'maximum_temperature':40, # maximum temperature
-        'maximum_irradiance':1000 # maximum irridation
+        'maximum_temperature':40, # maximum temperature [degreeC]
+        'maximum_irradiance':1000 # maximum irridation [W/m^2]
     }):
     """
     generates artificial Photo-Voltaic time series from temperature [degree Celsius] and irradiance [Watt/square meter] dataframes.
@@ -65,7 +65,9 @@ if __name__ == "__main__":
     temperature.columns = list(datasets.keys())
     irridation = pd.concat([datasets[loc][datasets[loc].columns[1]] for loc in list(datasets.keys())],1)
     irridation.columns = list(datasets.keys())
+
     # generate artifical loads:
-    photovoltaic_loads = generate_pv_series(temperature, irridation)
+    photovoltaic_generation = generate_pv_series(temperature, irridation)
+    
     # store data:
-    photovoltaic_loads.to_excel('generated loads.xlsx')
+    photovoltaic_generation.to_excel('PV generation.xlsx')
